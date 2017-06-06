f0cc4454ace44bd5d5e0b31fab191d6d0eef4e16
ysicing <root@ysicing.net>
2017-06-07 07:22:25 +0800

    update

--- app/auth.py
+++ app/auth.py
@@ -31,8 +31,8 @@ github = OAuth2Service(
     base_url='https://api.github.com/',
     access_token_url='https://github.com/login/oauth/access_token',
     authorize_url='https://github.com/login/oauth/authorize',
-    client_id= '',
-    client_secret= '',
+    client_id= '583923627cb8e77e6220',
+    client_secret= '640b3324fd38e5a53d404d924a45004dd1ec2fb4',
 )
 
 @auth.before_app_request
