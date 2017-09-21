ff6b9eddc27ee2b897ab20198d562ef1dfe257d5
ysicing <root@ysicing.net>
2017-09-21 19:24:38 +0800

    support get docker info

--- app/dash.py
+++ app/dash.py
@@ -8,11 +8,19 @@
 @time: 2017/9/20 22:46
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,jsonify
+from app.plugins.docker import DockerApi
 
 dash = Blueprint('dash', __name__)
+
+docker = DockerApi(host=None, timeout=None)
 
 
 @dash.route('/dash/')
 def dash_index():
     return render_template('dash.html')
+
+
+@dash.route('/dash/docker')
+def dash_docker_info():
+    return jsonify(docker.get_docker_version())
