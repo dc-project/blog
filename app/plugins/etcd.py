d90845058bbe092a5615bd2d24f40c69e8e36bfa
ysicing <root@ysicing.net>
2017-09-27 00:32:48 +0800

    support k8s etcd

---
+++ app/plugins/etcd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+@version:0.1
+@author: ysicing
+@file: blog/etcd.py
+@time: 9/27/17 12:29 AM
+"""
---
+++ app/plugins/k8sapi.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+@version:0.1
+@author: ysicing
+@file: blog/k8sapi.py
+@time: 9/27/17 12:30 AM
+"""
