b87000d96cbda89b8048c65f76ee823786a387ec
ysicing <root@ysicing.net>
2017-10-01 11:37:45 +0800

    update

--- env_python.sh
+++ env_python.sh
@@ -4,7 +4,9 @@ set -xe
 
 [ -d "/data/project" ] || mkdir -p /data/project/blog_env
 
-python3.6 -m venv /data/project/blog_env
+pip3 install virtualenv
+
+virtualenv -p /usr/bin/python3.6 /data/project/blog_env
 
 source /data/project/blog_env/bin/activate
 
